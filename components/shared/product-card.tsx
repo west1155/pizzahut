f111aa5bde68fe7b791b93c4d7fa5a2b3ebd005b
west1155 <westsupport@gmail.com>
2026-01-06 05:47:20 +0000

    close button fixed

--- components/shared/product-card.tsx
+++ components/shared/product-card.tsx
@@ -25,40 +25,42 @@ export const ProductCard: React.FC<ProductType> = ({ id, name, price, imgURL, it
     const [openModal, setOpenModal] = React.useState(false);
 
     return (
-        <div
-            className={cn('border border-gray-500-100 rounded-2xl p-4 hover:shadow-md transition-all duration-200 cursor-pointer', className)}
-            onClick={() => setOpenModal(true)}>
-            <div className={'flex justify-center p-6 bg-[#FEF3E2] rounded-lg h-65 group'}>
-                {imgURL ? (
-                    <Image
-                        src={imgURL}
-                        alt={name}
-                        width={215}
-                        height={215}
-                        className="object-contain transition-all duration-300 group-hover:scale-105
-                                group-hover:-translate-y-1
-                                group-active:scale-95
-                                group-active:translate-y-0"
-                    />
-                ) : (
-                    <div className="w-[215px] h-[215px] bg-gray-200 flex items-center justify-center rounded-lg">
-                        <span className="text-gray-400">No Image</span>
-                    </div>
-                )}
-            </div>
-            <Title text={name} size={'sm'} className={'mb-1 mt-3 font-bold'} />
-            <p className={'text-sm text-gray-400'}>
-                chicken, cheese, tomato sauce
-            </p>
-            <div className={'flex flex-wrap justify-between items-center mt-4 gap-2'}>
-                <span className={'text-[20px] whitespace-nowrap'}>
-                    from <b>{price} £ </b>
-                </span>
-                <Button variant="secondary"
-                    className="text-base font-bold bg-[#FEF3E2] text-orange-500 hover:bg-[#FEEBC8] transition-colors">
-                    <Plus className={'w-5 h-5 mr-1'} />
-                    Add to cart
-                </Button>
+        <div className={className}>
+            <div
+                className={cn('border border-gray-500-100 rounded-2xl p-4 hover:shadow-md transition-all duration-200 cursor-pointer h-full')}
+                onClick={() => setOpenModal(true)}>
+                <div className={'flex justify-center p-6 bg-[#FEF3E2] rounded-lg h-65 group'}>
+                    {imgURL ? (
+                        <Image
+                            src={imgURL}
+                            alt={name}
+                            width={215}
+                            height={215}
+                            className="object-contain transition-all duration-300 group-hover:scale-105
+                                    group-hover:-translate-y-1
+                                    group-active:scale-95
+                                    group-active:translate-y-0"
+                        />
+                    ) : (
+                        <div className="w-[215px] h-[215px] bg-gray-200 flex items-center justify-center rounded-lg">
+                            <span className="text-gray-400">No Image</span>
+                        </div>
+                    )}
+                </div>
+                <Title text={name} size={'sm'} className={'mb-1 mt-3 font-bold'} />
+                <p className={'text-sm text-gray-400'}>
+                    chicken, cheese, tomato sauce
+                </p>
+                <div className={'flex flex-wrap justify-between items-center mt-4 gap-2'}>
+                    <span className={'text-[20px] whitespace-nowrap'}>
+                        from <b>{price} £ </b>
+                    </span>
+                    <Button variant="secondary"
+                        className="text-base font-bold bg-[#FEF3E2] text-orange-500 hover:bg-[#FEEBC8] transition-colors">
+                        <Plus className={'w-5 h-5 mr-1'} />
+                        Add to cart
+                    </Button>
+                </div>
             </div>
 
             <ChooseProductModal
@@ -68,4 +70,4 @@ export const ProductCard: React.FC<ProductType> = ({ id, name, price, imgURL, it
             />
         </div>
     );
-}
+};
